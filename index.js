cebddbf73a0fb8fcb0215d44994ed8152e71aa1f
Damien Reimert <damien.reimert@gmail.com>
2021-10-15 16:45:45 +0200

    Mise à jour de la correction

--- index.js
+++ index.js
@@ -1,115 +1,138 @@
-const TikaServer = require("tika-server");
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const TikaServer = require('tika-server')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const Promise = require('bluebird')
 
 // Télécharger une page HTML
 const getHtml = (url) => {
-  return rp(url)
-  .then(function (htmlString) {
-    // console.log(htmlString);
-    return htmlString;
-  })
-  .catch(function (err) {
-    console.error("RP ERROR:", err)
-  });
+  if (url) {
+    return rp(url)
+      // en cas de réussite
+      .then(function (htmlString) {
+        // console.log(htmlString)
+        return htmlString
+      })
+      // en cas d'échec
+      .catch(function (err) {
+        console.error('getHtml :: RP ERROR:', err)
+      })
+  } else {
+    // si l'url est vide
+    console.error('getHtml :: url undefined')
+    return Promise.resolve(undefined)
+  }
 }
 
 // Télécharger un pdf
 const getPdf = (url) => {
-  return rp({
-    url: url,
-    encoding: null
-  }).then(function (pdf) {
-    // console.log("PDF download");
-    return pdf;
-  }).catch(function (err) {
-    console.error("RP ERROR:", err)
-  });
-}
-
-// Récupérer la liste des formations
-const getFormations = () => {
-  return getHtml('https://www.insa-lyon.fr/fr/formation/diplomes/ING').then((html) => {
-    const $ = cheerio.load(html);
-    // Pour obtenir le selecteur, j'explore la page avec mon navigateur
-    const urls = $('.diplome table a').map(function() {
-      return $(this).attr('href');
-    }).get();
-    return urls; // Ne pas oublier de retourner un résultat
-  })
-}
-
-const getPdfUrlsParcours = (url) => {
-  return getHtml(url).then((html) => {
-    const $ = cheerio.load(html);
-    const urls = $('#block-system-main .content-offre-formations table a').map(function() {
-      return $(this).attr('href');
-    }).get();
-    return urls; // Ne pas oublier de retourner un résultat
-  })
-}
-
-// Renvoie plus de 3000 urls
-const getAllPdfUrl = () => {
-  return getFormations().then((urls) => {
-    // Pour chaque url de formation, on récupère la liste des urls de pdf.
-    // Ici, je fais toutes les requetes en parallèle
-    // ce qui peut poser des problèmes si la machine n'a pas assez de mémoire.
-    return Promise.all(urls.map((url) => {
-      // url relative : /fr/formation/parcours/729/5/1
-      return getPdfUrlsParcours(`https://www.insa-lyon.fr${url}`)
-    })).then((urls) => {
-      // urls de la forme [[...], [...], [...]]
-      return [].concat.apply([], urls); // Applatissement du tableau
+  if (url) {
+    return rp({
+      url: url,
+      encoding: null // pour télécharger le binaire
+    }).then(function (pdf) {
+      // console.log('PDF download')
+      return pdf
+    }).catch(function (err) {
+      console.error('getPdf :: RP ERROR:', err)
     })
-  })
+  } else {
+    console.error('getPdf :: url undefined')
+    return Promise.resolve(undefined)
+  }
 }
 
-// Lance le serveur Tika
-// Doc : https://www.npmjs.com/package/tika-server
-const ts = new TikaServer();
-const db = {}
+const downloadAndAnalysePdf = (urls = ['http://planete.insa-lyon.fr/scolpeda/f/ects?id=36736&_lang=fr'], db = {}) => {
+  // Lance le serveur Tika
+  // Doc : https://www.npmjs.com/package/tika-server
+  const ts = new TikaServer()
 
-ts.on("debug", (msg) => {
-  // console.log(`DEBUG: ${msg}`)
-})
+  ts.on('debug', (msg) => {
+    // console.log(`DEBUG: ${msg}`)
+  })
 
-// Lance le serveur tika
-ts.start().then(() => {
-  return getAllPdfUrl().then((listeUrlPdfs) => {
+  // Lance le serveur tika
+  return ts.start().then(() => {
     // Pour chaque url ...
-    return listeUrlPdfs.reduce((p, url) => {
-      // Le reduce sert ici à éviter de faire toutes les requetes en même temps.
-      // Il y a plus de 3000 pdfs...
-      return p.then(() => {
-        // Extraction du texte.
-        // On vérifie qu'il y a bien une url
-        if (url) {
-          return getPdf(url).then((pdf) => {
-            // console.log("pdf", pdf);
-            if (pdf) {
-              // Et qu'il y a bien un pdf.
-              return ts.queryText(pdf).then((data) => {
-                // console.log(data)
-                const code = /CODE : ([^\n]*)/.exec(data)[1];
-                console.log("code :", code);
-                db[code] = true;
-              });
+    return Promise.reduce(urls, (db, url) => {
+      console.log(url);
+      // Extraction du texte.
+      return getPdf(url).then((pdf) => {
+        // console.log('pdf', pdf)
+        if (pdf) {
+          return ts.queryText(pdf).then((data) => {
+            const code = /CODE : ([^\n]*)/.exec(data)[1]
+            const ects = /ECTS : ([^\n]*)/.exec(data)[1]
+            const cours = /Cours : ([^\n]*) h/.exec(data)[1]
+            const td = /TD : ([^\n]*) h/.exec(data)[1]
+            const tp = /TP : ([^\n]*) h/.exec(data)[1]
+            const projet = /Projet : ([^\n]*) h/.exec(data)[1]
+            const pedagogique = /pédagogique : ([^\n]*) h/.exec(data)[1]
+            const personnel = /Travail personnel : ([^\n]*) h/.exec(data)[1]
+            const extraction = /CONTACT\n\n([^\n]*)\n([^\n]*)/.exec(data)
+
+            db[code] = {
+              code: code,
+              ects: ects,
+              cours: cours,
+              td: td,
+              tp: tp,
+              projet: projet,
+              pedagogique: pedagogique,
+              personnel: personnel,
+              prof: extraction[1],
+              email: extraction[2]
             }
+
+            return db
           })
+        } else {
+          return db
         }
       })
-    }, Promise.resolve()) // Initilise le reduce avec une promesse
+    }, db) // crée une base de données avec assoication test = 42. Mettre {} pour initiliser la db comme une DB vide.
+  }).then((db) => {
+    return ts.stop().then(() => db)
+  }).catch((err) => {
+    console.log(`TIKA ERROR: ${err}`)
   })
-}).then(() => {
-  return ts.stop()
-}).catch((err) => {
-  console.log(`TIKA ERROR: ${err}`, err)
-  return ts.stop()
-}).then(() => {
-  console.log(JSON.stringify(db, null, 2));
-})
+}
 
-// Version simple.
-// Il peut être bon de loguer les pages manquantes
-// pour les rapporter aux responsables.
+// Analyser du html
+// DOc : https://github.com/cheeriojs/cheerio
+// ou encore : https://github.com/sfrenot/competence/blob/master/formation/crawl.coffee
+const extractUrlPdfs = (url) => {
+  return getHtml(url).then((html) => {
+    const $ = cheerio.load(html)
+    const urls = $('#block-system-main .content-offre-formations table a').map(function () {
+      return $(this).attr('href')
+    }).get()
+    // console.log('urls:', urls)
+    return urls
+  })
+}
+
+const extractUrlFormations = (url = 'https://www.insa-lyon.fr/fr/formation/diplomes/ING') => {
+  return getHtml(url).then((html) => {
+    const $ = cheerio.load(html)
+    const urls = $('.diplome table tr td:nth-child(2) a').map(function () {
+      return $(this).attr('href')
+    }).get()
+    // console.log('urls:', urls)
+    return urls
+  })
+}
+
+// extractUrlPdfs('https://www.insa-lyon.fr/fr/formation/parcours/729/4/1').then((urls) => {
+//   downloadAndAnalysePdf(urls).then((db) => console.log(JSON.stringify(db, null, 2)))
+// })
+
+extractUrlFormations().then((formations) => {
+  return Promise.reduce(formations, (db, formation) => {
+    console.log(formation);
+    const url = 'https://www.insa-lyon.fr'+formation
+    console.log(url);
+    return extractUrlPdfs(url, db).then((urls) => {
+      return downloadAndAnalysePdf(urls)
+    })
+  }, {})
+}).then((db) => console.log(JSON.stringify(db, null, 2)))
